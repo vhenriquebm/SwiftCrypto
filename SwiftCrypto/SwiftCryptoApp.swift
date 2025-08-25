24f0125a900d046604d90c1b83b3c4758077c815
Vitor Marinho <89821794+vhenriquebm@users.noreply.github.com>
2025-08-25 15:29:15 +0100

    Initial Commit

---
+++ SwiftCrypto/SwiftCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCryptoApp.swift
+//  SwiftCrypto
+//
+//  Created by MARINHO Vitor on 25/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
