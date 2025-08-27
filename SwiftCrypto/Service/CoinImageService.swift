3c52d375a27d3c53986f0702950e16a5c35ecbe2
Vitor Marinho <89821794+vhenriquebm@users.noreply.github.com>
2025-08-27 09:10:39 +0100

    Add logic to save images for CoinImageService

--- SwiftCrypto/Service/CoinImageService.swift
+++ SwiftCrypto/Service/CoinImageService.swift
@@ -14,23 +14,49 @@ class CoinImageService {
     @Published var image: UIImage?
     private var imageSubscription: AnyCancellable?
     private let coin: CoinModel
+    private let fileManager = LocalFileManager.instance
+    private let folderName = "coin_images"
+    private let imageName: String
     
     init (coin: CoinModel) {
         self.coin = coin
+        self.imageName = coin.id
         getCoinImage()
     }
     
     private func getCoinImage() {
-        
+        if let savedImage = fileManager.getImage(imageName: imageName,
+                                                 folderName: folderName) {
+            image = savedImage
+            
+        } else {
+            downloadCoinImage()
+        }
+    }
+    
+    private func downloadCoinImage() {
         guard let url = URL(string: coin.image) else { return }
         
         imageSubscription = NetworkingManager.download(url: url)
-            .tryMap ( {(data) -> UIImage? in
-                return UIImage(data: data)
+            .tryMap { data -> UIImage in
+                guard let img = UIImage(data: data) else {
+                    throw URLError(.cannotDecodeContentData)
+                }
+                return img
             }
+            .receive(on: DispatchQueue.main)
+            .sink(
+                receiveCompletion: NetworkingManager.handleCompletion,
+                receiveValue: { [weak self] returnedImage in
+                    guard let self = self else { return }
+                    self.image = returnedImage
+                    self.imageSubscription?.cancel()
+                    self.fileManager.saveImage(
+                        image: returnedImage,
+                        imageName: self.imageName,
+                        folderName: self.folderName
+                    )
+                }
             )
-            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [ weak self ] returnedImage in
-                self?.image = returnedImage
-            })
     }
 }
